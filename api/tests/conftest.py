3716b1a48fac6077b0ab73de32828b8369712a76
Jimmy Chu <jchu@meilleursagents.com>
2024-10-22 19:36:20 +0200

    fix: fix config load for tests

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,14 +1,15 @@
 import pytest
-from dotenv import load_dotenv
 
+from secret_santa_api.db import db
 from secret_santa_api import create_app
 
 
 @pytest.fixture(scope="function")
 def app():
-    app = create_app(True)
+    app = create_app("./tests/.env.test")
 
     return app
+
 
 
 @pytest.fixture(scope="function")
@@ -16,13 +17,10 @@ def app_context(app):
     with app.app_context():
         yield app
 
+        db.session.remove()  # Remove any sessions
+        db.drop_all()  # Drop all tables
 
 @pytest.fixture(scope="function")
 def client(app):
     with app.test_client() as client:
         yield client
-
-
-@pytest.fixture(scope="session", autouse=True)
-def load_env():
-    load_dotenv(dotenv_path="./tests/.env.test")
